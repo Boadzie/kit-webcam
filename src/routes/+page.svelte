285f9c0e63119431506b2c8b3e341aaeeb10d6cc
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-02-09 10:02:35 +0000

    Add webcam function to app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+    let stream;
+    let videoRef;
+
+    async function getStream() {
+        try {
+            stream = await navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: false
+            });
+            videoRef.srcObject = stream;
+        } catch (err) {
+            console.error(err);
+        }
+        console.log(stream.getTracks()[0])
+    }
+
+    async function stopStream() {
+        stream.getTracks().forEach(track => track.stop());
+        videoRef.srcObject = null;
+    }
+</script>
